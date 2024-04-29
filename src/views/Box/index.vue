f0904550e61350f2a3f3d0270481cd6f83e8146b
huangge1199 <huangge1199@hotmail.com>
2024-04-29 10:34:38 +0800

    去掉box中原作者的GitHub信息

--- src/views/Box/index.vue
+++ src/views/Box/index.vue
@@ -23,7 +23,6 @@
     <div class="content">
       <!-- 可在此处自定义任意内容 -->
       <TimeCapsule />
-      <GithubProject />
     </div>
   </div>
 </template>
@@ -32,7 +31,6 @@
 import { CloseOne, SettingTwo } from "@icon-park/vue-next";
 import { mainStore } from "@/store";
 import TimeCapsule from "@/components/TimeCapsule.vue";
-import GithubProject from "@/components/GithubProject.vue";
 
 const store = mainStore();
 const closeShow = ref(false);
